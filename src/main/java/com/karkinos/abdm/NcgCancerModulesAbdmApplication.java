1cd152828fd1122582918b829cf198a478f3b079
kumarianamika001 <kumarianamika@karkinos.in>
2024-04-23 12:03:55 +0530

    Initial commit.
    Initial commit.

---
+++ src/main/java/com/karkinos/abdm/NcgCancerModulesAbdmApplication.java
@@ -0,0 +1,13 @@
+package com.karkinos.ncgcancermodulesabdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcgCancerModulesAbdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcgCancerModulesAbdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karkinos/ncgcancermodulesabdm/NcgCancerModulesAbdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karkinos.ncgcancermodulesabdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcgCancerModulesAbdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
